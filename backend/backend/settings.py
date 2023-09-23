aba31e5da666c009daeea01cc1d1585a7631f3d5
Agressor <super.guniv@yandex.ru>
2023-09-24 02:54:17 +0500

    update

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -7,7 +7,7 @@ SECRET_KEY = os.getenv("SECRET_KEY", default='token')
 
 DEBUG = os.getenv('DEBUG', default=False)
 
-ALLOWED_HOSTS = os.getenv('ALLOWED_HOSTS', default ='127.0.0.1, localhost').split()
+ALLOWED_HOSTS = os.getenv('ALLOWED_HOSTS', default='127.0.0.1, localhost').split()
 
 
 # Application definition
